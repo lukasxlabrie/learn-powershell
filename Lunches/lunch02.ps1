f574d1854fe2e4548c858fcc12c5dbb544fd75b5
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-06-22 10:41:01 -0400

    lab 02 outline

---
+++ Lunches/lunch02.ps1
@@ -0,0 +1,32 @@
+# Lab 02 consists of finding certain cmlets here is the list and the answers
+
+# how to update help
+Update-Help
+
+# cmdlet that converts cmdlet output into html
+
+# cmdlet that redirects output into a file
+
+# cmdlets for working with processess
+
+# cmldet used to set break point
+
+# cmdlets to create, modify, export and import aliases
+
+# How to keep a transcript of everything typed into the shell and save as text
+
+# How to get process by the name of process
+
+# How to have Get-Process tell you who started the process
+
+# Run a command on a remote host
+
+# Change character width parameter on the out-file cmdlet
+
+# Prevent overwritting while using the outfile cmdlet
+
+# Shortest way to get commands with process in the name
+
+# Number of gerneic object cmdlets
+
+# Find help about arrays
