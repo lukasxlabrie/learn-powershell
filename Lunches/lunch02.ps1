2ec2266364b58037d12e7382001cb876594939ac
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-06-24 14:09:52 -0400

    process cmdlets

--- Lunches/lunch02.ps1
+++ Lunches/lunch02.ps1
@@ -10,6 +10,15 @@ ConvertTo-Html
 Out-CurrentFile  
 
 # cmdlets for working with processess
+Enter-PSHostProcess               Cmdlet    Microsoft.PowerShell.Core Connects to and enters into an interactive session with a local process.
+Exit-PSHostProcess                Cmdlet    Microsoft.PowerShell.Core Closes an interactive session with a local process.
+Get-PSHostProcessInfo             Cmdlet    Microsoft.PowerShell.Core Gets process information about the PowerShell host.
+Switch-Process                    Cmdlet    Microsoft.PowerShell.Core On Linux and macOS, the cmdlet calls the #`execv()` function to provide similar b…
+Debug-Process                     Cmdlet    Microsoft.PowerShell.Man… Debugs one or more processes running on the local computer.
+Get-Process                       Cmdlet    Microsoft.PowerShell.Man… Gets the processes that are running on the local computer.
+Start-Process                     Cmdlet    Microsoft.PowerShell.Man… Starts one or more processes on the local computer.
+Stop-Process                      Cmdlet    Microsoft.PowerShell.Man… Stops one or more running processes.
+Wait-Process                      Cmdlet    Microsoft.PowerShell.Man… Waits for the processes to be stopped before accepting more input.
 
 # cmldet used to set break point
 
