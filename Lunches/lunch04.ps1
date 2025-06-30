81526dc07861bf326d9e2bc0db0a6519f289269b
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-06-30 15:16:46 -0400

    parameter alias

--- Lunches/lunch04.ps1
+++ Lunches/lunch04.ps1
@@ -3,3 +3,7 @@ Get-ExecutionPolicy
 
 # change execution policy
 Set-ExecutionPolicy
+
+# find parameter aliases
+PS /Users/james> (get-command get-process | select -Expandparameters).erroraction.aliases
+
