6b3476945af7a848b0b7479130474894d860a27b
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-07-10 15:06:42 -0400

    set env var, filter list

--- Lunches/lunch05.ps1
+++ Lunches/lunch05.ps1
@@ -1,2 +1,9 @@
 # lists all environment variables
 Get-ChildItem env:*
+
+# sets an environment variable 
+# Note: This will only set the variable for the current session
+Set-Item -Path Env:/A -Value 1
+
+# lists all environment variables starting with 'A'
+Get-ChildItem Env:/A*
