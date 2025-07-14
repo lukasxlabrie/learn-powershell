de7ee15f42f1cf3041d0309cd6f17544bae5939b
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-07-14 15:20:41 -0400

    lab template

--- Lunches/lunch05.ps1
+++ Lunches/lunch05.ps1
@@ -7,3 +7,22 @@ Set-Item -Path Env:/A -Value 1
 
 # lists all environment variables starting with 'A'
 Get-ChildItem Env:/A*
+
+#LABS
+# 1. Create a new directory called Labs.
+
+
+
+# 2. Create a zero-length file named /Labs/Test.txt (use New-Item).
+
+
+
+# 3. Is it possible to use Set-Item to change the contents of /Labs/Test.txt to -TESTING? Or do you get an error? If you get an error, why?
+
+
+
+# 4. Using the Environment provider, display the value of the system environment variable PATH.
+
+
+
+# 5. Use help to determine what the differences are between the -Filter, -Include, and -Exclude parameters of Get-ChildItem
