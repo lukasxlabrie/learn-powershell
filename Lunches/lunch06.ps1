7c55620c82efd49d837ff16fbb52487961830d5e
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-07-16 15:43:24 -0400

    JSON conversions

--- Lunches/lunch06.ps1
+++ Lunches/lunch06.ps1
@@ -3,3 +3,9 @@ Get-Process | Export-Csv processes.csv
 
 # Import the CSV file and display its content
 import-csv processes.csv
+
+# Export process as a JSON file
+Get-Process | ConvertTo-Json | Out-File procs.json
+
+# Import the JSON file and display its content
+Get-Content ./procs.json | ConvertFrom-Json
