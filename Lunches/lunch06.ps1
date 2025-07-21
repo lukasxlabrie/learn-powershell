e4d02b049c4707a3ada68b7f57d645bb19ad0f69
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-07-21 14:53:41 -0400

    removed exmaples

--- Lunches/lunch06.ps1
+++ Lunches/lunch06.ps1
@@ -1,16 +1,5 @@
-# Export process as a CSV file
-Get-Process | Export-Csv processes.csv
-
-# Import the CSV file and display its content
-import-csv processes.csv
-
-# Export process as a JSON file
-Get-Process | ConvertTo-Json | Out-File procs.json
-
-# Import the JSON file and display its content
-Get-Content ./procs.json | ConvertFrom-Json
-
 #LABS
+
 <# 1. Create two similar, but different, text files. Try comparing them by using Compare-
 Object. Run something like this: Compare-Object -Reference (Get-Content
 File1.txt) -Difference (Get-Content File2.txt). If the files have only one
