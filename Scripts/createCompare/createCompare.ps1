6656d6b4997b1a71ac30d169ca1b7fdef3584a40
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-07-28 14:10:45 -0400

    added write host commands

--- Scripts/createCompare/createCompare.ps1
+++ Scripts/createCompare/createCompare.ps1
@@ -2,17 +2,23 @@
 # This will create a log file in the specified path
 Start-Transcription -Path "C:\Users\lukas\OneDrive\Documents\learn-powershell\learn-powershell\Scripts\logs\createCompare.log"
 
+# adds text to the log file
+Write-Host "Starting the script to compare two text files..." -ForegroundColor Cyan
+
 # adds a timestamp to the log file
 Get-Date 
+Write-Host "Script started at: $(Get-Date)" -ForegroundColor Green
 
 # creates 2 new files in the current directory
 New-Item -Path . -Name "Example1.txt" -ItemType File
 New-Item -Path . -Name "Example2.txt" -ItemType File
+write-host "Created Example1.txt and Example2.txt" -ForegroundColor Yellow
 
 # writes content to the files, alternatively you can use 2 files you already have
 "Easy Peasey Lemon Squeezy" | Set-Content -Path ".\Example1.txt"
 "Easy Peasey Lemon Squeezy Curtail Simpson Target Breezey" | Set-Content -Path ".\Example2.txt"
 
 Compare-Object -ReferenceObject (Get-Content Example1.txt) -DifferenceObject (Get-Content Example2.txt)
+Write-Host "Compared Example1.txt and Example2.txt" -ForegroundColor Yellow
 
 Stop-Transcription
