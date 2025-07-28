b5817766e756138dbbc22d4d539d612e83ebba0a
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-07-28 14:20:43 -0400

    removed write-date and fixed transcript command

--- Scripts/createCompare/createCompare.ps1
+++ Scripts/createCompare/createCompare.ps1
@@ -1,13 +1,12 @@
 # Best practice: Always start with a transcription to log the script's output
 # This will create a log file in the specified path
-Start-Transcription -Path "C:\Users\lukas\OneDrive\Documents\learn-powershell\learn-powershell\Scripts\logs\createCompare.log"
+Start-Transcript -Path "C:\Users\lukas\OneDrive\Documents\learn-powershell\learn-powershell\Scripts\logs\createCompare.log"
 
 # adds text to the log file
 Write-Host "Starting the script to compare two text files..." -ForegroundColor Cyan
 
 # adds a timestamp to the log file
 Get-Date 
-Write-Host "Script started at: $(Get-Date)" -ForegroundColor Green
 
 # creates 2 new files in the current directory
 New-Item -Path . -Name "Example1.txt" -ItemType File
@@ -21,4 +20,4 @@ write-host "Created Example1.txt and Example2.txt" -ForegroundColor Yellow
 Compare-Object -ReferenceObject (Get-Content Example1.txt) -DifferenceObject (Get-Content Example2.txt)
 Write-Host "Compared Example1.txt and Example2.txt" -ForegroundColor Yellow
 
-Stop-Transcription
+Stop-Transcript
