3e028b6e4a0fbea0de436846e7cfd09831019a0b
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-08-25 15:15:40 -0400

    added ps1 file to lunch7, get module and add path + notes

---
+++ Lunches/lunch07.ps1
@@ -0,0 +1,7 @@
+# Install Az moduule if not already installed
+if (-not (Get-Module -ListAvailable -Name Az)) {
+    Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
+}
+
+# Add a custom module path to the PSModulePath environment variable
+$env:PSModulePath += [System.IO.Path]::PathSeparator + 'C:\Scripts/myModules'
