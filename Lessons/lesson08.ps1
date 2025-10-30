73ac39f4b18ecf5543496ec0ba5eaaff8743dc37
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-10-30 13:12:59 -0400

    labs for lesson 8

---
+++ Lessons/lesson08.ps1
@@ -0,0 +1,13 @@
+# 1. cmdlet for producing a radnom numbe
+
+# 2. cmdlet that displays current date/time
+
+# 3. the kind of object produced from the above
+
+# 4. use cmdlet from task 2 to and Select-Object to get only today
+
+# 5. cmdlet to show you all the times in the directory
+
+# 6. use cmdlet from task 5 to show times of your choice, sort by time, show only display name
+
+# 7. repeat the above but sort by last write time and display filename, creation time, and last write time, save as CSV and html
