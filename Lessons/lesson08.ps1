435fe620949d1bf165a60e47a3867579201f93b9
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-10-31 14:48:34 -0400

    dir access times

--- Lessons/lesson08.ps1
+++ Lessons/lesson08.ps1
@@ -11,9 +11,10 @@ Get-Date
 Get-Date |Select-Object -Property Day
 
 # 5. cmdlet to show you all the times in a directory
-Get-ChildItem "path to dir" | Select-Object Name, CreationTime, LastWriteTime, LastAccessTime
+Get-ChildItem "path to dir"  Select-Object Name, CreationTime, LastWriteTime, LastAccessTime
 
 # 6. use cmdlet from task 5 to show times of your choice, sort by time, show only display name
-
+Get-ChildItem "path to dir" | Sort-Object -CreationTime | Select-Object -ExpandProperty Name
 
 # 7. repeat the above but sort by last write time and display filename, creation time, and last write time, save as CSV and html
+####
