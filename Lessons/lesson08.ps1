2ccac286d12570de7377444466766dee0ded1001
lukasxlabrie <126988962+lukasxlabrie@users.noreply.github.com>
2025-10-31 14:54:36 -0400

    corrections and compelted lab 7

--- Lessons/lesson08.ps1
+++ Lessons/lesson08.ps1
@@ -5,16 +5,16 @@ Get-Random
 Get-Date 
 
 # 3. the kind of object produced from the above
-# Property
+# [System.DateTime]
 
 # 4. use cmdlet from task 2 to and Select-Object to get only today
 Get-Date |Select-Object -Property Day
 
 # 5. cmdlet to show you all the times in a directory
-Get-ChildItem "path to dir"  Select-Object Name, CreationTime, LastWriteTime, LastAccessTime
+Get-ChildItem "path to dir" | Select-Object Name, CreationTime, LastWriteTime, LastAccessTime
 
 # 6. use cmdlet from task 5 to show times of your choice, sort by time, show only display name
 Get-ChildItem "path to dir" | Sort-Object -CreationTime | Select-Object -ExpandProperty Name
 
 # 7. repeat the above but sort by last write time and display filename, creation time, and last write time, save as CSV and html
-####
+Get-ChildItem "path to dir" | Select-Object Name, CreationTime, LastWriteTime | Sort-Object -LastWriteTime | Export-Csv -Path "output.csv" -NoTypeInformation $data | ConvertTo-Html | Out-File "output.html"
